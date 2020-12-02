f9d125b5afa3b5263dfc26437ca240d9307eb8d4
bastiannAn <an.quezadae@alumnos.duoc.cl>
2020-12-01 21:29:16 -0300

    Generacion de Artista Controller y metodo ObtenerArtistas con retorno de Json

---
+++ ApiMusica/Controllers/ArtistaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiMusica.Azure;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiMusica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistaController : ControllerBase
+    {
+        
+        // GET: api/<ArtistaController>/all
+        [HttpGet("all")]
+        public JsonResult ObtenerArtistas()
+        {
+            var artistasRecibidos = MusicaAzure.ObtenerArtistas();
+            return new JsonResult(artistasRecibidos);
+        }
+        
+       
+    }
+}
